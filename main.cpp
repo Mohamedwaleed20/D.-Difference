1922b5b5864a684754aa3dd73d0d5aab9ddef7a4
Shagee3.eg2004 <168550015+Mohamedwaleed20@users.noreply.github.com>
2024-08-10 21:44:18 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long a , b , c , d ;
+    cin >> a >> b >> c >> d ;
+    long long x = (a*b) - (c*d);
+    cout << "Difference = " << x ;
+    return 0;
+}
